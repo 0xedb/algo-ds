9eb6d19da6e4cb43b05efe2278377f2a6ed78f74
theBashShell <edohbruno@gmail.com>
2019-12-30 23:53:29 +0000

    so far so good with array

--- javascript/array.js
+++ javascript/array.js
@@ -12,8 +12,13 @@
 
 class CustomArray {
   constructor() {
-    this._capacity = 10;
+    this._capacity = 5;
     this._size = 0;
+    this._array = {};
+
+    for (let i = 0; i < this._capacity; i++) {
+      this._array[i] = null;
+    }
   }
 
   get size() {
@@ -23,7 +28,35 @@ class CustomArray {
   get capacity() {
     return this._capacity;
   }
+
+  get loadfactor() {
+    return this.size / this.capacity;
+  }
+
+  push(data) {
+    if (this.loadfactor > 0.7) {
+      console.log("resizing...");
+      const newArray = { ...this._array };
+      const newCapacity = this._capacity * 2;
+      for (let i = this._capacity; i < newArray; i++) {
+        newArray[i] = null;
+      }
+      this._capacity = newCapacity;
+      this._array = newArray;
+    }
+
+    // else just add to the end
+    this._array[this._size++] = data;
+  }
+
+  at(index) {
+    if(index < this.capacity) {
+      return this._array[index]
+    } else {
+      throw Error("index out of bounds")
+    }
+  }
+
 }
 
-
-module.exports = CustomArray
+module.exports = CustomArray;
--- javascript/tests/array.test.js
+++ javascript/tests/array.test.js
@@ -1,8 +1,20 @@
-const CustomArray = require('../array')
+const CustomArray = require("../array");
 
-const myArray = new CustomArray()
+const myArray = new CustomArray();
 
 test("size of new array", () => {
-  expect(myArray.size).toBe(0)
+  expect(myArray.size).toBe(0);
+});
+
+test("3rd item is 3", () => {
+  myArray.push(100);
+  myArray.push(20);
+  myArray.push(3);
+  myArray.push(3) 
+  myArray.push(3) 
+  expect(myArray.at(2)).toBe(3); 
+});
+
+test("capacity to be 10", () => {
   expect(myArray.capacity).toBe(10)
 })
