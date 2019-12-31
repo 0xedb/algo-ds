df4a86c079f0dd76eb423ee6fe8d0acef5c9d626
theBashShell <edohbruno@gmail.com>
2019-12-31 00:34:07 +0000

    more tests passed

--- javascript/tests/array.test.js
+++ javascript/tests/array.test.js
@@ -10,21 +10,23 @@ test("3rd item is 32", () => {
   myArray.push(100);
   myArray.push(20);
   myArray.push(32);
-  myArray.push(323) 
-  myArray.push(13) 
-  myArray.pop()
-  expect(myArray.at(2)).toBe(32); 
+  myArray.push(323);
+  myArray.push(13);
+  myArray.pop();
+  expect(myArray.at(2)).toBe(32);
 });
 
 test("capacity to be 5", () => {
-  expect(myArray.capacity).toBe(5)
-})
+  expect(myArray.capacity).toBe(5);
+});
 
 test("size to be 4", () => {
-  expect(myArray.size).toBe(4)
-})
+  expect(myArray.size).toBe(4);
+});
 
 test("find 32", () => {
-  expect(myArray.find(32)).toBe(2) 
-})
-
+  expect(myArray.find(32)).toBe(2);
+  myArray.pop();
+  expect(myArray.find(13)).toBe(-1);
+  expect(myArray.at(0)).toBe(100);
+});
