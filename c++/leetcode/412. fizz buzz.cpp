cf5a1eb7d0843aca25765b5685ec3ce1a1aac814
theBashShell <edohbruno@gmail.com>
2020-01-11 01:02:47 +0000

    lc c++ fizzbuzz"

---
+++ c++/leetcode/412. fizz buzz.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        for(int i = 1; i <= n; i++){
+            string str = ""; 
+                 if( i % 3 == 0) {
+                     str += "Fizz"; 
+                 }
+                 if (i % 5 == 0) {
+                     str += "Buzz"; 
+                 }
+                 if(!(i % 3 == 0 || i % 5 == 0)) {
+                     str += to_string(i);
+                 }
+            
+            ans.push_back(str);
+        }
+        return ans;
+    }
+};
