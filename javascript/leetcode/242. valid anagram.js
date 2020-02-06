4a95045a5e1fbf1d34410028f6e5005449e299e7
theBashShell <edohbruno@gmail.com>
2020-02-06 23:07:07 +0000

    lc valid anagram naive in js

---
+++ javascript/leetcode/242. valid anagram.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if(s.length !== t.length) return false
+    
+    const mp = new Map()
+    for(let val of s){
+        if(mp.has(val)) {
+            mp.set(val, mp.get(val) + 1)
+            continue
+        }
+        mp.set(val, 1)
+    }
+    
+    for(let val of t){
+        if(mp.has(val)) {
+            mp.set(val, mp.get(val) - 1)
+            continue
+        } 
+        return false
+    }
+    
+    for(let val of t){
+        if(mp.get(val) !== 0) return false    
+    }
+    
+    return true
+    
+    
+};
