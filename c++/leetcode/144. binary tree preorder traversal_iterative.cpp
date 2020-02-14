5c6218a754e0f6358f4ab3d8f060ba25b15f2644
theBashShell <edohbruno@gmail.com>
2020-02-14 13:16:21 +0000

    lc binary tree postorder traversal c++ iterative

---
+++ c++/leetcode/144. binary tree preorder traversal_iterative.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+  vector<int> preorderTraversal(TreeNode *root)
+  {
+    vector<int> array;
+    if (root == nullptr)
+      return array;
+
+    stack<TreeNode *> stk;
+    stk.push(root);
+
+    while (!stk.empty())
+    {
+      // pop
+      TreeNode *removed = stk.top();
+      stk.pop();
+      array.push_back(removed->val);
+
+      // push if right
+      if (removed->right)
+      {
+        stk.push(removed->right);
+        preorderTraversal(removed->right);
+      }
+
+      // push if left
+      if (removed->left)
+      {
+        stk.push(removed->left);
+        preorderTraversal(removed->left);
+      }
+    }
+
+    return array;
+  }
+};
