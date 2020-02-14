faa3c21a8e6d9b645b0d1231c3cf8011442f505e
theBashShell <edohbruno@gmail.com>
2020-02-14 15:05:22 +0000

    lc binary tree postorder traversal c++ iterative corrected

--- c++/leetcode/144. binary tree preorder traversal_iterative.cpp
+++ c++/leetcode/144. binary tree preorder traversal_iterative.cpp
@@ -1,3 +1,4 @@
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -13,6 +14,7 @@ public:
   vector<int> preorderTraversal(TreeNode *root)
   {
     vector<int> array;
+
     if (root == nullptr)
       return array;
 
@@ -21,26 +23,64 @@ public:
 
     while (!stk.empty())
     {
-      // pop
       TreeNode *removed = stk.top();
       stk.pop();
       array.push_back(removed->val);
 
-      // push if right
       if (removed->right)
-      {
         stk.push(removed->right);
-        preorderTraversal(removed->right);
-      }
-
-      // push if left
       if (removed->left)
-      {
         stk.push(removed->left);
-        preorderTraversal(removed->left);
-      }
     }
 
     return array;
   }
 };
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//  * };
+//  */
+// class Solution
+// {
+// public:
+//   vector<int> preorderTraversal(TreeNode *root)
+//   {
+//     vector<int> array;
+//     if (root == nullptr)
+//       return array;
+
+//     stack<TreeNode *> stk;
+//     stk.push(root);
+
+//     while (!stk.empty())
+//     {
+//       // pop
+//       TreeNode *removed = stk.top();
+//       stk.pop();
+//       array.push_back(removed->val);
+
+//       // push if right
+//       if (removed->right)
+//       {
+//         stk.push(removed->right);
+//         preorderTraversal(removed->right);
+//       }
+
+//       // push if left
+//       if (removed->left)
+//       {
+//         stk.push(removed->left);
+//         preorderTraversal(removed->left);
+//       }
+//     }
+
+//     return array;
+//   }
+// };
+
+
