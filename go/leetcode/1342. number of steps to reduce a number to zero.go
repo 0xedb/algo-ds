6057b5b852406346bf89039f27566869563e6027
theBashShell <edohbruno@gmail.com>
2020-02-14 00:40:42 +0000

    lc reduce to zero go

---
+++ go/leetcode/1342. number of steps to reduce a number to zero.go
@@ -0,0 +1,17 @@
+func numberOfSteps (num int) int { 
+    
+    steps := 0
+    
+    for num > 0 {
+        if num % 2 == 0 {
+            num = num / 2
+            steps++
+        } else {
+            num = num - 1
+            steps ++
+        }
+    }
+    
+    return steps
+    
+}
