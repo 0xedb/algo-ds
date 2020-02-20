c93fdc7bcbb0e3af9117a9dd26cf16fb83a63a42
theBashShell <edohbruno@gmail.com>
2020-02-20 23:17:21 +0000

    tree corrected

--- javascript/trees.js
+++ javascript/trees.js
@@ -8,8 +8,8 @@ class Node {
 
 
 let sll = new Node(10);
-sll.right = new Node(-1);
-sll.left = new Node(21);
+sll.left = new Node(-1);
+sll.right = new Node(21);
 sll.left.left = new Node(-10);
 sll.left.right = new Node(4)
 sll.right.left = new Node(15);
@@ -18,4 +18,4 @@ sll.right.right.right = new Node(45);
 sll.right.right.right.right = new Node(60);
 
 
-console.table(sll)
+console.dir(sll)
