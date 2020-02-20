4b486e0519de92ec1927636d10d1fe742d71dca5
theBashShell <edohbruno@gmail.com>
2020-02-20 23:11:50 +0000

    tree structured

--- javascript/trees.js
+++ javascript/trees.js
@@ -1,5 +1,21 @@
 class Node {
   constructor(data) {
     this.data = data
+    this.right = null
+    this.left = null
   }
 }
+
+
+let sll = new Node(10);
+sll.right = new Node(-1);
+sll.left = new Node(21);
+sll.left.left = new Node(-10);
+sll.left.right = new Node(4)
+sll.right.left = new Node(15);
+sll.right.right = new Node(30);
+sll.right.right.right = new Node(45);
+sll.right.right.right.right = new Node(60);
+
+
+console.table(sll)
