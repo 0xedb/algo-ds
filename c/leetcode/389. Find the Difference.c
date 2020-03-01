4a339ef30ac231d101308066bdde5da6d7711390
theBashShell <edohbruno@gmail.com>
2020-03-01 23:53:46 +0000

    find the difference in c

---
+++ c/leetcode/389. Find the Difference.c
@@ -0,0 +1,17 @@
+
+
+char findTheDifference(char * s, char * t){
+    int sum_s = 0;
+    int sum_t = 0;
+    
+    for(int i = 0; i < strlen(s); i++) {
+        sum_s += s[i];
+    }
+    
+    for(int i = 0; i < strlen(t); i++) {
+        sum_t += t[i];
+    }
+    
+    return sum_t - sum_s;
+}
+
