d2a4cb06d793a3f8dc57087d2b80081295bf1558
theBashShell <edohbruno@gmail.com>
2020-03-04 14:32:40 +0000

    more tests for binary search

--- javascript/binaryseach.js
+++ javascript/binaryseach.js
@@ -1,21 +1,21 @@
-let arr = [0, 1, 2, 3, 5, 93, 1000];
+let arr = [-1000, 0, 1, 2, 5, 93];
 
 function bSearch(ar, num) {
-  console.table(ar)
-  if(ar.length < 1) return false
+  console.table(ar);
+  if (ar.length < 1) return false;
   let mid = Number.parseInt(ar.length / 2);
 
-  if(ar[mid] === num) return true
+  if (ar[mid] === num) return true;
 
-  if(ar[mid] < num) {
-    return bSearch(ar.slice(mid + 1), num)
+  if (ar[mid] < num) {
+    return bSearch(ar.slice(mid + 1), num);
   } else {
-    return bSearch(ar.slice(0, mid), num)
+    return bSearch(ar.slice(0, mid), num);
   }
-  
+
   return false;
 }
 
 console.log();
-console.log(bSearch(arr, 3));
+console.log(bSearch(arr, -1000));
 console.log();
