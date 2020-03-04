52e123e19449cfda014b4a4ec5e62dd8dff9a990
theBashShell <edohbruno@gmail.com>
2020-03-04 14:29:17 +0000

    rusty binary search

---
+++ javascript/binaryseach.js
@@ -0,0 +1,21 @@
+let arr = [0, 1, 2, 3, 5, 93, 1000];
+
+function bSearch(ar, num) {
+  // console.log(ar)
+  if(ar.length < 1) return false
+  let mid = Number.parseInt(ar.length / 2);
+
+  if(ar[mid] === num) return true
+
+  if(ar[mid] < num) {
+    return bSearch(ar.slice(mid + 1), num)
+  } else {
+    return bSearch(ar.slice(0, mid), num)
+  }
+  
+  return false;
+}
+
+console.log();
+console.log(bSearch(arr, 2));
+console.log();
