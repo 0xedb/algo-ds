ac706ec27656d1e480d829da078dbc0c5b2d7c8e
theBashShell <edohbruno@gmail.com>
2020-03-07 02:40:37 +0000

    insert into BST in go

---
+++ go/leetcode/701. Insert into a Binary Search Tree.go
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+    cur := root
+    
+    for cur != nil {
+        if val > cur.Val {
+            if cur.Right == nil {
+                cur.Right = new(TreeNode)
+                cur.Right.Val = val
+                return root
+            }
+            cur = cur.Right
+        }
+        
+        if val < cur.Val {
+            if cur.Left == nil {
+                cur.Left = new (TreeNode)
+                cur.Left.Val = val
+                return root
+            }
+            cur = cur.Left
+        }
+    }
+    
+    return root
+}
