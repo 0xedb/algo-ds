f4c759a9a474230aeb50d30a44a07348fc5057f0
theBashShell <edohbruno@gmail.com>
2020-03-07 02:07:17 +0000

    insert into BST in c++

--- c++/leetcode/701. Insert into a Binary Search Tree.cpp
+++ c++/leetcode/701. Insert into a Binary Search Tree.cpp
@@ -42,3 +42,53 @@ public:
     return root;
   }
 };
+
+
+
+// about 40% improvement in runtime
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+  TreeNode *insertIntoBST(TreeNode *root, int val)
+  {
+    TreeNode *cur = root;
+
+    while (cur)
+    {
+      if (val > cur->val)
+      {
+        if (cur->right)
+        {
+          cur = cur->right;
+          continue;
+        }
+        cur->right = new TreeNode(val);
+        return root;
+      }
+
+      if (val < cur->val)
+      {
+        if (cur->left)
+        {
+          cur = cur->left;
+          continue;
+        }
+
+        cur->left = new TreeNode(val);
+        return root;
+      }
+    }
+
+    return root;
+  }
+};
