681fb93ac984f2fa3dc3512d1aa0bbc141d4220a
theBashShell <edohbruno@gmail.com>
2020-03-08 23:49:36 +0000

    lc pow in c

---
+++ c/leetcode/50. Pow(x, n)d.c
@@ -0,0 +1,18 @@
+
+
+double myPow(double x, int n)
+{
+  if (n == 0)
+    return 1;
+
+  double partial = myPow(x, n / 2);
+
+  if (n % 2 == 0)
+    return partial * partial;
+  else
+  {
+    if (n < 0)
+      return (partial * partial) / x;
+    return x * partial * partial;
+  }
+}
