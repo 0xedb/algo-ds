d744f40e3c3b451ccf53abe9dcf617d04b959209
theBashShell <edohbruno@gmail.com>
2020-03-15 01:35:25 +0000

    included all

---
+++ c/leetcode/1342. Number of Steps to Reduce a Number to Zero.c
@@ -0,0 +1,15 @@
+
+
+int numberOfSteps(int num)
+{
+  if (num == 0)
+    return 0;
+  int count = 0;
+  if (num % 2 == 0)
+  {
+    count = 1 + numberOfSteps(num / 2);
+  }
+  else
+    count = 1 + numberOfSteps(num - 1);
+  return count;
+}
