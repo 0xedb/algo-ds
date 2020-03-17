f5dc79dc5a596decc047b358ef15a96dd295221e
theBashShell <edohbruno@gmail.com>
2020-03-17 21:16:58 +0000

    lc list intersection

---
+++ c++/leetcode/160. Intersection of Two Linked Lists.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+  ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+  {
+    if (!headA || !headB)
+      return nullptr;
+
+    ListNode *first = headA;
+    ListNode *second = headB;
+
+    while (first != second)
+    {
+      first = first->next;
+      second = second->next;
+
+      if (first == second)
+      {
+        return first;
+      }
+
+      if (!first)
+        first = headB;
+      if (!second)
+        second = headA;
+    }
+
+    return first;
+  }
+};
