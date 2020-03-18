758d0af6c9c9ed0fb5a2cdb686d63e2abbb9d756
theBashShell <edohbruno@gmail.com>
2020-03-18 22:42:23 +0000

    lc bst iterator in js

--- javascript/leetcode/173. Binary Search Tree Iterator.js
+++ javascript/leetcode/173. Binary Search Tree Iterator.js
@@ -46,3 +46,52 @@ BSTIterator.prototype.inorder = function(root) {
  * var param_1 = obj.next()
  * var param_2 = obj.hasNext()
  */
+
+
+ // delete root initialization for performance boost
+ /**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */ 
+/**
+ * @param {TreeNode} root
+ */
+var BSTIterator = function(root) { 
+    this._sorted = [];
+    this._pos = 0;
+    this.inorder(root)   
+};
+
+/**
+ * @return the next smallest number
+ * @return {number}
+ */
+BSTIterator.prototype.next = function() {
+     return this._sorted[this._pos++];
+};
+
+/**
+ * @return whether we have a next smallest number
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function() {
+    return this._pos < this._sorted.length
+};
+
+BSTIterator.prototype.inorder = function(root) {
+     if(!root) return  
+    
+    this.inorder(root.left) 
+    this._sorted.push(root.val)
+    this.inorder(root.right)
+}
+
+/** 
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
