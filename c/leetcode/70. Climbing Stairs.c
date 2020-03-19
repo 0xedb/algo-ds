b8fc4f01dd980ab5b472ff01fedd20986692b23d
theBashShell <edohbruno@gmail.com>
2020-03-19 19:46:10 +0000

    lc climbing stairs in c

---
+++ c/leetcode/70. Climbing Stairs.c
@@ -0,0 +1,16 @@
+
+
+int climbStairs(int n)
+{
+  if (n == 1)
+    return 1;
+  int first = 1;
+  int second = 2;
+  for (int i = 3; i <= n; i++)
+  {
+    int third = second + first;
+    first = second;
+    second = third;
+  }
+  return second;
+}
