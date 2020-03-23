ae7d6841486710797197814bde0fbcf393d2bcf8
theBashShell <edohbruno@gmail.com>
2020-03-23 11:54:32 +0000

    lc single el in sorted array in js

---
+++ javascript/leetcode/540. Single Element in a Sorted Array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = left + (right - left) / 2;
+    let isEven = (right - mid) % 2 == 0;
+
+    if (nums[mid] == nums[mid - 1]) {
+      if (isEven) right = mid - 2;
+      else left = mid + 1;
+    } else if (nums[mid] == nums[mid + 1]) {
+      if (isEven) left = mid + 2;
+      else right = mid - 1;
+    } else return nums[mid];
+  }
+  return nums[left];
+};
