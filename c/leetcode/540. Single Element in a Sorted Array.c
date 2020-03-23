bcfb60c39d7610e45fdc8f78dc73b89ae0dda413
theBashShell <edohbruno@gmail.com>
2020-03-23 11:37:13 +0000

    lc single el in sorted array in c

---
+++ c/leetcode/540. Single Element in a Sorted Array.c
@@ -0,0 +1,39 @@
+
+
+int singleNonDuplicate(int *nums, int numsSize)
+{
+  int left = 0;
+  int right = numsSize - 1;
+
+  while (left < right)
+  {
+    int mid = left + (right - left) / 2;
+    bool isEven = (right - mid) % 2 == 0;
+    if (nums[mid] == nums[mid - 1])
+    {
+      if (isEven)
+      {
+        right = mid - 2;
+      }
+      else
+      {
+        left = mid + 1;
+      }
+    }
+    else if (nums[mid] == nums[mid + 1])
+    {
+      if (isEven)
+      {
+        left = mid + 2;
+      }
+      else
+      {
+        right = mid - 1;
+      }
+    }
+    else
+      return nums[mid];
+  }
+
+  return nums[left];
+}
