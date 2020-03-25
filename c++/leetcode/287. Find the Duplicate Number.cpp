04dcacd1b4903ca6b35057874386752a7818ff07
theBashShell <edohbruno@gmail.com>
2020-03-25 00:05:50 +0000

    lc guess no high low in c++

---
+++ c++/leetcode/287. Find the Duplicate Number.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+  int findDuplicate(vector<int> &nums)
+  {
+    set<int> st;
+
+    for (int i : nums)
+    {
+      if (st.count(i))
+        return i;
+      else
+        st.insert(i);
+    }
+
+    return 1;
+  }
+};
