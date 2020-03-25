5973abd8ed5a2531731b2b59edebaf55845ff8fd
theBashShell <edohbruno@gmail.com>
2020-03-25 20:53:34 +0000

    lc 1st bad in c++

---
+++ c++/leetcode/278. First Bad Version.cpp
@@ -0,0 +1,25 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution
+{
+public:
+  int firstBadVersion(int n)
+  {
+    if (n == 1)
+      return 1;
+    int start = 1;
+
+    while (start < n)
+    {
+      int mid = start + ((n - start) / 2);
+      bool bad = isBadVersion(mid);
+      if (bad)
+        n = mid;
+      else
+        start = mid + 1;
+    }
+
+    return start;
+  }
+};
