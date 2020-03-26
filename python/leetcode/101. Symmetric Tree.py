6926db381bacba9857b0a50d73cb0a754433cd76
theBashShell <edohbruno@gmail.com>
2020-03-26 20:59:18 +0000

    lc symmetric tree in py

---
+++ python/leetcode/101. Symmetric Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root is None:
+            return True
+        return self.check2(root.left, root.right)
+    
+    def check2(self, left, right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None:
+            return False
+        return left.val == right.val and self.check2(left.right, right.left) and self.check2(left.left, right.right)
+        
+        
+  
