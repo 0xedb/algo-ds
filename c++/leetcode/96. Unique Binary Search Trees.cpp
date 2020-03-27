67a3828bfe013e6c5dcea5e684e805e413c61f8d
theBashShell <edohbruno@gmail.com>
2020-03-27 23:00:09 +0000

    lc unique BST in c++

---
+++ c++/leetcode/96. Unique Binary Search Trees.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+  int numTrees(int n)
+  {
+    vector<int> ans(n + 1, 0);
+    ans[0] = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+      for (int j = 1; j <= i; j++)
+      {
+        ans[i] += ans[i - j] * ans[j - 1];
+      }
+    }
+
+    return ans[n];
+  }
+};
