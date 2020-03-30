8890304cb558ce866d661e12b7d27503210333e5
theBashShell <edohbruno@gmail.com>
2020-03-30 12:37:00 +0000

    lc jewels and stones in go

--- go/leetcode/771. Jewels and Stones.go
+++ go/leetcode/771. Jewels and Stones.go
@@ -9,3 +9,17 @@ func numJewelsInStones(J string, S string) int {
     }
     return count
 }
+
+
+///
+import "strings"
+
+func numJewelsInStones(J string, S string) int {
+    count := 0
+    
+    for i := 0; i < len(S); i++ {
+        if(strings.Contains(J, string(S[i]))) {count++}
+    }
+    
+    return count
+}
