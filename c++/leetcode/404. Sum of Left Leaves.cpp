551ac01fc528109ea09eca354236eb47c7f7dcb1
theBashShell <edohbruno@gmail.com>
2020-04-08 21:29:53 +0000

    lc sum of left leaves in c++

---
+++ c++/leetcode/404. Sum of Left Leaves.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+  int sum = 0;
+
+public:
+  int sumOfLeftLeaves(TreeNode *root)
+  {
+    if (!root)
+      return 0;
+    if (root->left && !root->left->left && !root->left->right)
+    {
+      return root->left->val += sumOfLeftLeaves(root->right);
+    }
+
+    return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+  }
+};
