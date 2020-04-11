a747bc1d0314cd7f9eb2af9547bc4e145ef8f3fe
theBashShell <edohbruno@gmail.com>
2020-04-11 10:24:37 +0000

    fixed first missing positive c++

--- c++/leetcode/41. First Missing Positive.cpp
+++ c++/leetcode/41. First Missing Positive.cpp
@@ -25,3 +25,46 @@ public:
     return min;
   }
 };
+
+
+// correct and O(n)
+class Solution
+{
+public:
+  int firstMissingPositive(vector<int> &nums)
+  {
+    if (nums.empty())
+      return 1;
+    const int size = nums.size();
+    bool seen_one = false;
+
+    // remove invalid : all < 1 and size
+    for (int i = 0; i < size; ++i)
+    {
+      if (nums[i] == 1 && !seen_one)
+        seen_one = true;
+      else if (nums[i] < 1 || nums[i] > size)
+        nums[i] = 1;
+    }
+
+    if (!seen_one)
+      return 1;
+
+    // mark visited indices
+    for (int i = 0; i < size; ++i)
+    {
+      const int ind = abs(nums[i]) - 1;
+      if (nums[ind] > 0)
+        nums[ind] *= -1;
+    }
+
+    // find missing
+    for (int i = 0; i < size; ++i)
+    {
+      if (nums[i] > 0)
+        return i + 1;
+    }
+
+    return size + 1;
+  }
+};
