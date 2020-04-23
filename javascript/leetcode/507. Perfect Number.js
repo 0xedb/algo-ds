84bf5f3427c5082eeb4fea12a05ef21bb75e2cbd
theBashShell <edohbruno@gmail.com>
2020-04-23 15:58:20 +0000

    lc perfect number in js

---
+++ javascript/leetcode/507. Perfect Number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(num) {
+    if(num < 2) return false;
+    
+    let sum = 1
+    
+    for(let i = 2; i * i <= num; ++i) {
+        if(num % i == 0)
+            sum += i == parseInt(num / i) ? i : i + parseInt(num / i)
+        if(sum > num)
+            return false
+    }
+    
+    return sum == num
+};
