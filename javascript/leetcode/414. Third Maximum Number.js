492ab3598f91467689a6f814ccee31f293be75ba
theBashShell <edohbruno@gmail.com>
2020-04-24 23:05:58 +0000

    lc third maximum number in js

---
+++ javascript/leetcode/414. Third Maximum Number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    let _max = 0
+    
+    const seen = new Set(nums) 
+    
+    if(seen.size < 3) return Math.max(...seen)
+    
+    for(let i = 0; i < 3; ++i) {
+        _max = Math.max(...seen)
+        seen.delete(_max)
+    }
+    
+    return _max
+};  
