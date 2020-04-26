a7ed16d54c4bd0519166af8615d2710c92fc43fd
theBashShell <edohbruno@gmail.com>
2020-04-26 01:16:47 +0000

    lc base 10 complement in c++

---
+++ c++/leetcode/1009. Complement of Base 10 Integer.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+  int bitwiseComplement(int N)
+  {
+    int mask = 1;
+
+    while (mask < N)
+    {
+      mask = (mask << 1) | 1;
+    }
+
+    return mask ^ N;
+  }
+};
