0e9fdcf4f02d6ff81fad5bfab3014ccf2c3ad3db
theBashShell <edohbruno@gmail.com>
2020-05-04 23:49:26 +0000

    lc maximum product of 3 numbers in js

---
+++ javascript/leetcode/628. Maximum Product of Three Numbers.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function (nums) {
+  const sz = nums.length;
+
+  const first = nums[0] * nums[1] * nums[sz - 1];
+  const second = nums[sz - 3] * nums[sz - 2] * nums[sz - 1];
+
+  return Math.max(first, second);
+};
